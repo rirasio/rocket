70bb20ecebb129bd22a6548c94cf35cab6a30f1a
rirasio <chlwlsgud23@gmail.com>
2021-01-15 12:16:20 +0900

    basic setting

---
+++ src/test/java/com/rocket/rocket/RocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocket.rocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
