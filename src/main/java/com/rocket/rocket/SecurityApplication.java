e0328c26c2d1f63c3df1b4268669526a92df318d
ehddn <ehddn@192.168.123.106>
2021-01-17 01:12:16 +0900

    security add

--- src/main/java/com/rocket/rocket/SecurityApplication.java
+++ src/main/java/com/rocket/rocket/SecurityApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
-public class RocketApplication {
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+public class SecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RocketApplication.class, args);
+		SpringApplication.run(SecurityApplication.class, args);
 	}
 
 }
