bb2b2098419ce3ec7075811cfeea7bed79727f4a
rirasio <chlwlsgud23@gmail.com>
2021-01-17 22:56:14 +0900

    마이바티스 연동, Security설정 RocketApplication 합침

--- src/test/java/com/rocket/rocket/RocketApplicationTests.java
+++ src/test/java/com/rocket/rocket/RocketApplicationTests.java
@@ -1,13 +1,26 @@
 package com.rocket.rocket;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 class RocketApplicationTests {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testSqlSession() throws Exception {
+		System.out.println(sqlSession.toString());
 	}
 
 }
