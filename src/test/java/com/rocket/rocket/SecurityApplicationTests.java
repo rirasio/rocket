a1ad91501091a322afe3ea231d11d6527e9ce01b
rirasio <chlwlsgud23@gmail.com>
2021-01-18 08:52:28 +0900

    securityapplication 복구 multiple 해결

--- src/test/java/com/rocket/rocket/SecurityApplicationTests.java
+++ src/test/java/com/rocket/rocket/SecurityApplicationTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-class RocketApplicationTests {
+class SecurityApplicationTests {
 	
 	@Autowired
 	private SqlSessionTemplate sqlSession;
