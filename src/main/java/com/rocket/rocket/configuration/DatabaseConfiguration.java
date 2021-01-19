d0ec0e7316ed6c2964d1190f985a287f0246f03e
rirasio <chlwlsgud23@gmail.com>
2021-01-19 11:26:50 +0900

    마이바티스 - 오라클 데이터 읽기 (Lec)

--- src/main/java/com/rocket/rocket/configuration/DatabaseConfiguration.java
+++ src/main/java/com/rocket/rocket/configuration/DatabaseConfiguration.java
@@ -5,35 +5,39 @@ import javax.sql.DataSource;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
+@MapperScan(basePackages = "rocket.rocket.mapper")
+@EnableTransactionManagement
 @PropertySource("classpath:/application.properties")
 public class DatabaseConfiguration {
 	
 	@Autowired
 	private ApplicationContext applicationContext;
 	
-	@Bean
-	@ConfigurationProperties(prefix="spring.datasource.hikari")
-	public HikariConfig hikariConfig() {
-		return new HikariConfig();
-	}
-	
-	@Bean
-	public DataSource dataSource() throws Exception{
-		DataSource dataSource = new HikariDataSource(hikariConfig());
-		System.out.println(dataSource.toString());
-		return dataSource;
-	}
+//	스프링부트 2.1 이상의 경우 hikariCP를 자동으로 사용합니다. 그러므로 별도 설정이 필요없습니다.	
+//	@Bean
+//	@ConfigurationProperties(prefix="spring.datasource.hikari")
+//	public HikariConfig hikariConfig() {
+//		return new HikariConfig();
+//	}
+//	@Bean
+//	public DataSource dataSource() throws Exception{
+//		DataSource dataSource = new HikariDataSource(hikariConfig());
+//		System.out.println(dataSource.toString());
+//		return dataSource;
+//	}
 	
 	@Bean
 	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
