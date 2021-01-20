e78322e39460bb632689fb3bb7ff2cf99543f1f8
rirasio <chlwlsgud23@gmail.com>
2021-01-20 19:11:05 +0900

    패키지 구조 변경

---
+++ src/main/java/com/rocket/rocket/service/LecService.java
@@ -0,0 +1,21 @@
+package com.rocket.rocket.service;
+
+import java.util.List;
+
+import com.rocket.rocket.domain.LecVO;
+
+public interface LecService {
+	
+	public void register(LecVO lecvo);
+	
+	public LecVO get(String lec_num);
+	
+	public boolean modify(LecVO lecvo);
+	
+	public boolean remove(String lec_num);
+	
+	public List<LecVO> getList();
+	
+	
+
+}
