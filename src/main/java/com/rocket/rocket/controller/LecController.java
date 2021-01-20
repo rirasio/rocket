c748c1e94aa8d9fc4728d8bac801185191a6b405
rirasio <chlwlsgud23@gmail.com>
2021-01-20 16:10:23 +0900

    렉처 CRUD 구현

--- src/main/java/com/rocket/rocket/controller/LecController.java
+++ src/main/java/com/rocket/rocket/controller/LecController.java
@@ -1,9 +1,11 @@
 package com.rocket.rocket.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.rocket.rocket.domain.LecVO;
@@ -12,22 +14,54 @@ import com.rocket.rocket.service.lec.LecService;
 import lombok.AllArgsConstructor;
 
 @Controller
-@RequestMapping("/lec/*")
+@RequestMapping("/lecture")
 @AllArgsConstructor
 public class LecController {
 
-	private final LecService lecService;
+	private final LecService service;
 	
-	@GetMapping(value = {"/registerLec"})
-	public String registerLec() {
-		return "lec/registerLec";
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("list", service.getList());
+		return "lecture/list";
 	}
 	
-	@PostMapping(value = {"/registerLec"})
+	@GetMapping(value = {"/register"})
+	public String registerLec() {
+		return "lecture/register";
+	}
+	
+	@PostMapping(value = {"/register"})
 	public String registerLec(LecVO lecvo, RedirectAttributes rttr) {
-		lecService.register(lecvo);
+		service.register(lecvo);
 		rttr.addFlashAttribute("result", lecvo.getLec_num());
-		return "test";
+		return "redirect:/lecture/list";
+	}
+	
+	@GetMapping({"/get", "/modify"})
+	public void get(@RequestParam("lec_num") String lec_num, Model model) {
+		model.addAttribute("lecture", service.get(lec_num));
+	}
+	
+//	@GetMapping("/modify")
+//	public String modify() {
+//		return "lecture/modifyForm";
+//	}
+	
+	@PostMapping("/modify")
+	public String modify(LecVO lecvo, RedirectAttributes rttr) {
+		if (service.modify(lecvo)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/lecture/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("lec_num") String lec_num, RedirectAttributes rttr) {
+		if (service.remove(lec_num)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/lecture/list";
 	}
 
 }
--- src/main/java/com/rocket/rocket/service/lec/LecServiceImpl.java
+++ src/main/java/com/rocket/rocket/service/lec/LecServiceImpl.java
@@ -1,6 +1,9 @@
 package com.rocket.rocket.service.lec;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
 import com.rocket.rocket.domain.LecVO;
@@ -10,15 +13,39 @@ import lombok.AllArgsConstructor;
 import lombok.Setter;
 
 @Service
+@Primary
 @AllArgsConstructor
 public class LecServiceImpl implements LecService {
 
 	@Setter(onMethod_ = @Autowired)
-	private LecMapper lecMapper;
+	private LecMapper mapper;
 	
 	@Override
 	public void register(LecVO lecvo) {
-		lecMapper.insert(lecvo);
+		mapper.insert(lecvo);
 	}
+
+	@Override
+	public LecVO get(String lec_num) {
+		mapper.updateHitCount(lec_num);
+		return mapper.select(lec_num);
+	}
+
+	@Override
+	public boolean modify(LecVO lecvo) {
+		return mapper.update(lecvo) == 1;
+	}
+
+	@Override
+	public boolean remove(String lec_num) {
+		return mapper.delete(lec_num) == 1;
+	}
+
+	@Override
+	public List<LecVO> getList() {
+		return mapper.getList();
+	}
+	
+	
 
 }
