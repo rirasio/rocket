4e378b492ee420b72c20455699a23f32ca8bb080
Renz <Renz@DESKTOP-LQ70MLP>
2021-01-28 09:53:33 +0900

    SecurityConfig랑 Security Domain에 VO를 구동 가능하게 수정하였습니다.

--- src/main/java/com/rocket/rocket/configuration/SecurityConfig.java
+++ src/main/java/com/rocket/rocket/configuration/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
@@ -45,7 +44,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public UserDetailsService customUserService() {
 		return new CustomUserDetailsService();
 	}
-	
 
 	@Bean
 	public AuthenticationSuccessHandler loginSuccessHandler() {
@@ -62,11 +60,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// static 디렉터리의 하위 파일 목록은 인증 무시 ( = 항상통과 ) >> 지금 모든 페이지 다 무시하게 해놓음
 		web.ignoring().antMatchers("/resources/**"); // "/css/**", "/js/**", "/img/**", "/lib/**" 등등
 	}
-	//권한 아이디설정
+
+	// 권한 아이디설정
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 		log.info("권한 읽기 시작--------");
-		
 
 //		auth.userDetailsService(customUserService()).passwordEncoder(passwordEncoder());
 //		try {
@@ -84,23 +82,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		
 //		test용 임시 계정
 
-		//하드코딩 비밀번호는 1111입니다.
-		String epassword = new BCryptPasswordEncoder().encode("1111");		
+		// 하드코딩 비밀번호는 1111입니다.
+		String epassword = new BCryptPasswordEncoder().encode("1111");
 		auth.inMemoryAuthentication().withUser("rocketbot1").password(epassword).roles("9");
 		auth.inMemoryAuthentication().withUser("rocketbot2").password(epassword).roles("1");
 		auth.inMemoryAuthentication().withUser("rocketbot3").password(epassword).roles("2");
 		auth.inMemoryAuthentication().withUser("rocketbot4").password(epassword).roles("0");
-		
+
 //		auth.inMemoryAuthentication().withUser("rocketbot1").password("{noop}rocketbot1").roles("9");
 //		auth.inMemoryAuthentication().withUser("rocketbot1").password("$2a$10$kmCUFCNxf0LDqy2OKKdGkuKY7dnZTk.X9/y9vAYtTl8vp9VT4gzs6").roles("9");
 //		auth.inMemoryAuthentication().withUser("rocketbot2").password("{noop}rocketbot2").roles("2");
 //		auth.inMemoryAuthentication().withUser("rocketbot3").password("{noop}rocketbot3").roles("1");
 //		auth.inMemoryAuthentication().withUser("rocketbot4").password("{noop}rocketbot4").roles("0");
 //		auth.inMemoryAuthentication().withUser("rocketbot5").password("{noop}rocketbot5").roles("2,9");
-		
-	}	
 
-
+	}
 
 //	@Override
 //	public void configure(WebSecurity web) throws Exception {
@@ -113,7 +109,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http.authorizeRequests()
-				// 페이지 권한 설정(큰권한이 제일 상단에 있어야됨)
+		// 페이지 권한 설정(큰권한이 제일 상단에 있어야됨)
 
 //				.antMatchers("/users/admin/**").hasRole("ADMIN")// 관리자
 //				.antMatchers("/users/admin/**").access("hasRole('ADMIN')")// 관리자
@@ -125,51 +121,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers("/users/subs/**").hasRole("1")// 구독회원
 				.antMatchers("/users/student/**").hasRole("0")// 일반회원
 				.antMatchers("/**").permitAll()// 비회원까지
-				.anyRequest().authenticated()
-				;
+				.anyRequest().authenticated();
 
-				// 로그인 설정
-				http.formLogin().
-				loginPage("/users/login")//로그인 컨트롤러와 일치
-				.loginProcessingUrl("/users/login")//form action이랑 일치
+		// 로그인 설정
+		http.formLogin().loginPage("/users/login")// 로그인 컨트롤러와 일치
+				.loginProcessingUrl("/users/login")// form action이랑 일치
 //				.defaultSuccessUrl("/")
 //				.usernameParameter("email")
 //				.passwordParameter("pw")
-				//.successHandler(loginSuccessHandler())//@Bean loginSuccessHandler를 의존성 주입해서 가져왔음
-				//.defaultSuccessUrl("/users/logout")
-				;
-				// 로그아웃 설정
-				http.logout().logoutUrl("/users/logout")//로그아웃을 요청할 경로이고 기본값은 /logout											
-				.invalidateHttpSession(true); 
-				
-				// 403 예외처리 핸들링
-				http.exceptionHandling().accessDeniedPage("/users/access_denied");// 로그인 실패
-				//http.logoutRequestMatcher(new AntPathRequestMatcher("/users/logout")) //로그아웃의 기본 URL(/logout) 이 아닌 다른 URL로 재정의합니다.
-				//http.logoutSuccessUrl("/users/logout")// 로그아웃이 처리된 후 이동될 경로이고 >> 기본값은/login?logout >> 첫페이지 보낼때 사용할 수 있음
+		// .successHandler(loginSuccessHandler())//@Bean loginSuccessHandler를 의존성 주입해서
+		// 가져왔음
+		// .defaultSuccessUrl("/users/logout")
+		;
+		// 로그아웃 설정
+		http.logout().logoutUrl("/users/logout")// 로그아웃을 요청할 경로이고 기본값은 /logout
+				.invalidateHttpSession(true);
+
+		// 403 예외처리 핸들링
+		http.exceptionHandling().accessDeniedPage("/users/access_denied");// 로그인 실패
+		// http.logoutRequestMatcher(new AntPathRequestMatcher("/users/logout")) //로그아웃의
+		// 기본 URL(/logout) 이 아닌 다른 URL로 재정의합니다.
+		// http.logoutSuccessUrl("/users/logout")// 로그아웃이 처리된 후 이동될 경로이고 >>
+		// 기본값은/login?logout >> 첫페이지 보낼때 사용할 수 있음
 
 		// http.formLogin().loginPage("/customLogin").loginProcessingUrl("/login");
 
+		// http.csrf().ignoringAntMatchers("/**");
 
-		//http.csrf().ignoringAntMatchers("/**");
-
-		http.sessionManagement()
-       .sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
 
 	}
 
-
-	@Bean
-	public UserDetailsService customUserService() {
-		return new CustomUserDetailsService();
-	}
-
-	@Bean
-	public AuthenticationSuccessHandler loginSuccessHandler() {
-		return new CustomLoginSuccessHandler();// loginSuccess사용하기위함
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
 }
