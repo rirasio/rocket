65b3a352b699ac182d0d53d62940e9b15d55d3dc
admin <admin@DESKTOP-PA383OT>
2021-01-30 16:42:39 +0900

    qqqqqq

--- src/main/java/com/rocket/rocket/controller/ClassController.java
+++ src/main/java/com/rocket/rocket/controller/ClassController.java
@@ -30,26 +30,28 @@ public class ClassController {
 
 	
 	//	디폴트 리스트 
-	@GetMapping(value = { "/list" })
-	public String list(Model model) {
-
-		log.info("list page");
-		model.addAttribute("classlist", classService.classList());
-		model.addAttribute("ctgylist", classService.ctgyList());
-
-		return "classes/list";
-	}
+//	@GetMapping(value = { "/list" })
+//	public String list(Model model) {
+//
+//		log.info("list page");
+//		model.addAttribute("classlist", classService.classList());
+//		model.addAttribute("ctgylist", classService.ctgyList());
+//
+//		return "classes/list";
+//	}
 
 	
 ////////////////////////////////////////////////////////////////////////////////////////////////////	
 	
 		
 	//	필터 적용 리스트
-	@GetMapping(value = { "/listCtgy" })
-	public void listCtgy(@RequestParam("{{ctgy_title}}") String ctgy_title, Model model) {
+	@GetMapping(value = { "/list" })
+	public void listCtgy(Criteria cri, Model model) {
 
 		log.info("filtered list page");
-		model.addAttribute("filter", classService.classListCTGY(ctgy_title));
+		model.addAttribute("classlist", classService.classList(cri));
+		model.addAttribute("ctgylist", classService.ctgyList());
+		int total = classService.getTotal(cri);
 	}
 
 	
